eb3cb7f9d358379b82de5bc8431f41c0f84d7235
Aakash Manihar <111000108+Aakash-ak07@users.noreply.github.com>
2023-01-31 13:05:13 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,15 @@ import React from 'react'
 import '../styles/App.css';
 
 const App = () => {
-  const query = new URLSearchParams(window.location.search);
-  const id = query.get("id");
-  const name = query.get("name");
-  const designation = query.get("designation");
+  const queryParams = new URLSearchParams(window.location.search);
+  const id = queryParams.get("id");
+  const name = queryParams.get("name");
+  const designation = queryParams.get("designation");
 
   return(
     <div className="App">
       <div className="details">
-        <p>Employee{id} named{name} works as{designation}</p>
+        <p>Employee {id} named {name} works as {designation}</p>
       </div>
     </div>
   )
